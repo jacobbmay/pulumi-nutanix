b6d4a304c3ccff78aa3160e7cc108d68e26f929a
Jacob May <jacob.may@defenseunicorns.com>
2023-09-28 19:33:27 -0400

    Confirmed working example project

--- examples/simple-nutanix-example/ts/index.ts
+++ examples/simple-nutanix-example/ts/index.ts
@@ -1,32 +1,74 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as std from "@pulumi/std";
 import * as nutanix from "@jacobbmay/nutanix";
-import { input } from "@jacobbmay/nutanix/types";
-import { VirtualMachineDiskList } from "@jacobbmay/nutanix/types/input";
-
-const provider = new nutanix.Provider('nutanix', {
-    endpoint: process.env.PRISM_CENTRAL_ENDPOINT,
-    port: "9440",
-    username: process.env.PRISM_CENTRAL_USERNAME,
-    password: process.env.PRISM_CENTRAL_PASSWORD,
-    insecure: true,
-    waitTimeout: 10,
-    sessionAuth: true
-  });
 
 let config = new pulumi.Config();
 
-const clusters = nutanix.getClusters();
-const ubuntuImage = nutanix.getImage({imageName: "Ubuntu Server"});
+const provider = new nutanix.Provider('nutanix', {
+    endpoint: config.require("PRISM_CENTRAL_ENDPOINT"),
+    port: "443",
+    username: config.require("PRISM_CENTRAL_USERNAME"),
+    password: config.requireSecret("PRISM_CENTRAL_PASSWORD"),
+    insecure: false,
+    waitTimeout: 10,
+    sessionAuth: true
+});
 
+const imageId = nutanix.getImage({imageName: config.require("imageName")}, {provider: provider}).then((res) => {
+  return res.id
+});
+const clusterId = nutanix.getCluster({name: config.require("clusterName")}, {provider: provider}).then((res) => {
+  return res.clusterId
+});
+const subnetId = nutanix.getSubnet({subnetName: config.require("subnetName")}, {provider: provider}).then((res) => {
+  return res.id
+});
 
-const test_vm = new nutanix.VirtualMachine('test_vm', {
-  name: "test_vm",
-  clusterUuid: clusters.then((res) => {
-    return res.entities[0].metadata.clusterUuid
-  }),
-  numVcpusPerSocket: 2,
-  numSockets: 1,
-  memorySizeMib: 4096,
-  powerStateMechanism: "OFF",
-  // diskLists: input<VirtualMachineDiskList>
-  });
+const pubKey = config.require("pubkey")
+const joinToken = "examplejointoken123456"
+
+const cloudConfig = `
+#cloud-config
+hostname: bootstrap
+users:
+  - name: cloud-user
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    shell: /bin/bash
+    ssh_authorized_keys:
+    - ${pubKey}
+ssh_pwauth: False
+runcmd:
+  - |
+    server_ip=$(ip route get $(ip route show 0.0.0.0/0 | grep -oP 'via \\K\\S+') | grep -oP 'src \\K\\S+')
+    /root/rke2-startup.sh -t ${joinToken} -T cluster.foo.bar -s \${server_ip} -u cloud-user
+`;
+
+  const rke2Bootstrap = new nutanix.VirtualMachine("rke2_bootstrap", {
+    name: `bootstrap-node`,
+    clusterUuid: clusterId,
+    memorySizeMib: 8192,
+    numSockets: 1,
+    numVcpusPerSocket: 8,
+    diskLists: [
+        {
+            dataSourceReference: {
+                kind: "image",
+                uuid: imageId
+            },
+            // deviceProperties: {
+            //     diskAddress: { adapterType: "SCSI", deviceIndex: "0"},
+            //     deviceType: "DISK",
+            // },
+            diskSizeMib: 102400,
+        },
+        {
+            diskSizeMib: 102400,
+        },
+    ],
+    nicLists: [{
+        subnetUuid: subnetId,
+    }],
+    guestCustomizationCloudInitUserData: std.base64encodeOutput({
+        input: cloudConfig,
+    }).result,
+}, {provider: provider});
